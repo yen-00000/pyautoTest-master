ba55035e50d0aaf54097602c9ad2b90837ab8419
Yen <yen2020@qq.com>
2020-08-30 00:43:38 +0800

    修改报告存放路径

--- run_tests.py
+++ run_tests.py
@@ -6,6 +6,7 @@ import pytest
 import click
 from conftest import REPORT_DIR
 from config import RunConfig
+import shutil
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -23,8 +24,8 @@ def init_env(new_report):
     """
     初始化测试报告目录
     """
-    os.mkdir(new_report)
-    os.mkdir(new_report + "/image")
+    # os.mkdir(new_report)
+    # os.mkdir(new_report + "/image")
 
 
 
@@ -33,14 +34,19 @@ def init_env(new_report):
 def run(m):
     if m is None or m == "run":
         logger.info("回归模式，开始执行✈✈！")
-        now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
-        RunConfig.NEW_REPORT = os.path.join(REPORT_DIR, now_time)
+        # now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+        NEW_REPORT=REPORT_DIR+"Allure_report"
+        if os.path.exists(NEW_REPORT):
+            shutil.rmtree(NEW_REPORT)
+        else:
+            pass
+        RunConfig.NEW_REPORT = os.path.join(REPORT_DIR, "Allure_report")
         init_env(RunConfig.NEW_REPORT)
         pytest.main(["-s", "-v", RunConfig.cases_path,
                      "--alluredir",RunConfig.NEW_REPORT+'/json',
                      "--maxfail", RunConfig.max_fail,
                      "--reruns", RunConfig.rerun])
-        os.system('allure generate '+RunConfig.NEW_REPORT+'/json'+' -o ' + RunConfig.NEW_REPORT+'/html')
+        os.system('allure generate '+RunConfig.NEW_REPORT+'/json'+' -o ' + RunConfig.NEW_REPORT+'/html'+ ' --clean')
         logger.info("运行结束，生成测试报告♥❤！")
     elif m == "debug":
         print("debug模式，开始执行！")
